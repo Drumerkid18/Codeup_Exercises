45e88755fb4feaf990b666cd236e076190b25fa4
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-11-21 11:04:07 -0600

    Created if/else/elseif statements, and Used Ternary Operators

--- if.php
+++ if.php
@@ -44,4 +44,47 @@ if ($b !== $c) {
     echo "$b is not identical to $c\n";
 }
 
+
+
+
+// Shorten the next 2 statements into an if/else
+echo $b < $c ? "$b is less than $c\n" : "$b is greater than $c\n";
+
+// this is the same as this
+    // if ($b < $c) {
+    //     // output the appropriate result
+    //     echo "$b is less than $c\n";
+
+    // } else {
+    //     echo "$b is greater than $c\n";
+    //}
+
+// Shorten the next 2 statements into an if/else
+echo $a >= $c ? "$a is greater than or equal to $c\n" : "$a is less than $c\n";
+
+    // if ($a >= $c) {
+    //     // output the appropriate result
+    //     echo "$a is greater than or equal to $c\n";
+
+    // } else {
+    //     echo "$a is less than $c\n";
+    // }
+
+// combine the next 4 conditionals into
+// one if/else/elseif block that checks in order for:
+// identical, equal, not identical, not equal
+if ($a == $c) {
+    // output the appropriate result
+    echo "$a is equal to $c\n";
+
+} elseif ($a === $c) {
+    echo "$a is identical to $c\n";
+
+} elseif ($a != $c) {
+    echo "$a is not equal to $c\n";
+
+} else {
+    echo "$a is not identical to $c\n";
+}
+
 ?>
