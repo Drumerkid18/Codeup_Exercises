7a83122ae1372b36775c72b21aba024d2c3e2085
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-11-24 15:51:30 -0600

    Created user imput for Incrementer, and set default

--- for.php
+++ for.php
@@ -5,6 +5,16 @@ $low = trim(fgets(STDIN));
 echo "Please enter Ending Number\n";
 $high = trim(fgets(STDIN));
 
-for ($low; $low <= $high; $low++){
+echo "Would you like to set Increment Value\n";
+$yes_or_no = trim(fgets(STDIN));
+
+if ($yes_or_no == 'yes'){
+		echo "Please enter Increment Value\n";
+		$incrementer = trim(fgets(STDIN));
+}else{
+	$incrementer = 1;
+}
+
+for ($low; $low <= $high;$low += $incrementer){
 	echo "\$low has a value of {$low}\n";
 }
