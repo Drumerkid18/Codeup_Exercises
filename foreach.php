2f033a2837ed3862a8ddbea964359e05c6265dc8
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-11-25 12:53:03 -0600

    created foreach loop to show data type

---
+++ foreach.php
@@ -0,0 +1,19 @@
+<?php
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach($things as $value){
+	if(is_int($value)){
+		echo "Integer\n";
+	}elseif(is_float($value)){
+		echo "Float\n";
+	}elseif(is_bool($value)){
+		echo 'Boolean' . PHP_EOL;
+	}elseif(is_array($value)){
+		echo "Array\n";
+	}elseif(is_null($value)){
+		echo 'NULL' . PHP_EOL;
+	}else{
+		echo "String\n";
+	}
+}
