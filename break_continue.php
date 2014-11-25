5aae3b50c069ad5a5ead1953b94b43e780aadc28
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-11-25 15:58:26 -0600

    added "break" at 10

--- break_continue.php
+++ break_continue.php
@@ -1,8 +1,13 @@
 <?php
 
 for ($num = 1; $num <= 100; $num++) {
-	if($num % 2 == 1){
-		continue;
+	echo "$num \n";
+	
+	if($num == 10){
+		break;
 	}
-		echo "$num \n";
+
+	// if($num % 2 == 1){
+	// 	continue;
+	// }
 }
