bc7f9f22a2f55ec208e69334bcee2201dba4fb9b
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-11-26 15:05:21 -0600

    added error messages for non numeric values or 0

--- arithmetic.php
+++ arithmetic.php
@@ -1,31 +1,56 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+    if(is_numeric($a) && is_numeric($b)){
+    	echo $a + $b . PHP_EOL;
+    }else {
+    	echo "ERROR: Both arguments mus be numeric\n";
+    }
 }
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)){
+    	echo $a - $b . PHP_EOL;
+    }else {
+    	echo "ERROR: Both arguments mus be numeric\n";
+    }
 }
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)){
+    	echo $a * $b . PHP_EOL;
+    }else {
+    	echo "ERROR: Both arguments mus be numeric\n";
+    }
 }
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+    if(is_numeric($a) && is_numeric($b)){
+    	if($a == 0.0 || $b == 0.0){
+    		echo "ERROR: Attempt to divide by 0\n";
+    	}else{
+    		echo $a / $b . PHP_EOL;
+    	}	
+    }else {
+    	echo "ERROR: Both arguments mus be numeric\n";
+    }
 }
 
 function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)){
+    	echo $a % $b . PHP_EOL;
+    }else {
+    	echo "ERROR: Both arguments mus be numeric\n";
+    }
 }
 
-add(3, 5);
+add('taco', 5);
 
-subtract(50, 20);
+subtract('migas', 20);
 
-multiply(5, 10);
+multiply('tortas', 10);
 
-divide(30, 10);
+divide(0, 10);
 
-modulus(15, 4);
+modulus('rice', 4);
+
