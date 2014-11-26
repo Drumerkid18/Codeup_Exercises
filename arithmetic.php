04545d213b69c75ed40a58f43fa49669a688e507
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-11-26 13:40:46 -0600

    created functions

---
+++ arithmetic.php
@@ -0,0 +1,25 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b) {
+    echo $a - $b . PHP_EOL;
+}
+
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
+}
+
+function divide($a, $b) {
+    echo $a / $b . PHP_EOL;
+}
+
+add(3, 5);
+
+subtract(50, 20);
+
+multiply(5, 10);
+
+divide(30, 10);
