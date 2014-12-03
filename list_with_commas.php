2cd9d8686d2067bcf0054027ef1997ce9962b05a
Travis French <travisfrench@Traviss-MacBook-Pro.local>
2014-12-03 11:24:53 -0600

    added first name sorting

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,17 +1,16 @@
 <?php
+
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark, Neil DeGrasse Tyson';
 
 function humanizedList($anyString){
 
-
 	$anyArray = explode(', ', $anyString);
+	sort($anyArray);
 	$lastItem = array_pop($anyArray);
-	// var_dump($physicistsArray);
-
 	array_push($anyArray, 'and ' . $lastItem);
 	$anyString = implode(', ', $anyArray);
+
 	return $anyString;
-	// var_dump($famousFakePhysicists);
 }
 
 $famousFakePhysicists = humanizedList($physicistsString);
