e6d7016d17228015d5ac6884e4ef1b7e910b158d
Travis French <travster217@gmail.com>
2015-07-22 11:55:19 -0500

    updated black jack

--- black_jack.php
+++ black_jack.php
@@ -6,6 +6,20 @@ $cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 
 // build a deck (array) of cards
 // card values should be "VALUE SUIT". ex: "7 H"
 // make sure to shuffle the deck before returning it
+function getInput($upper = true){
+
+    // Capture user input
+    $cleaninput = trim(fgets(STDIN));
+
+    // Only sometimes will we modify the input to uppercase, before returning. 
+    if ($upper) {
+        $cleaninput = strtoupper($cleaninput);
+    }
+    
+    // Return user input 
+    return $cleaninput;
+ }
+
 function buildDeck($suits, $cards) {
 	foreach ($suits as $suit) {
 		foreach ($cards as $card) {
@@ -30,9 +44,9 @@ function cardIsAce($card) {
 // face cards are worth 10
 // numeric cards are worth their value
 function getCardValue($card) {
-	$getCardValue = 0
-	$cardArray[0] = explode(' ', $card);
-	switch ($cardArry) {
+	$getCardValue = 0;
+	$cardArray = explode(' ', $card);
+	switch ($cardArray[0]) {
 		case 'Ace':
 			$getCardValue += 11;
 			break;
@@ -59,16 +73,21 @@ function getCardValue($card) {
 // don't forget to factor in aces
 // aces can be 1 or 11 (make them 1 if total value is over 21)
 function getHandTotal($hand) {
+	$handTotal = 0;
 	foreach ($hand as $card) {
-		if($card){
-
-		}elseif()
+		$cardValue = getCardValue($card);
+		$handTotal += $cardValue;
 	}
+	return $handTotal;
 }
 // draw a card from the deck into a hand
 // pass by reference (both hand and deck passed in are modified)
 function drawCard(&$hand, &$deck) {
-  // todo
+	$randomCard = mt_rand(0, count($deck) -1);
+	$selectedCard = $deck[$randomCard];
+	array_push($hand, $selectedCard);
+	unset($deck, $selectedCard);
+	return $hand;
 }
 // print out a hand of cards
 // name is the name of the player
@@ -78,7 +97,16 @@ function drawCard(&$hand, &$deck) {
 // or:
 // Player: [J D] [2 D] Total: 12
 function echoHand($hand, $name, $hidden = false) {
-  // todo
+	foreach ($hand as $card) {
+		if ($hidden) {
+			echo $name . " hand: " . $card . PHP_EOL;
+			break;
+		}else {
+			echo $name . " hand: " . $card . PHP_EOL;
+			continue;
+		}
+	}
+
 }
 // build the deck of cards
 $deck = buildDeck($suits, $cards);
@@ -86,31 +114,72 @@ $deck = buildDeck($suits, $cards);
 $dealer = [];
 $player = [];
 // dealer and player each draw two cards
-// todo
+$dealer = drawCard($dealer, $deck);
+$player = drawCard($player, $deck);
+$dealer = drawCard($dealer, $deck);
+$player = drawCard($player, $deck);
 // echo the dealer hand, only showing the first card
-// todo
-// echo the player hand
-// todo
-// allow player to "(H)it or (S)tay?" till they bust (exceed 21) or stay
-while (todo) {
-  // todo
-}
-// show the dealer's hand (all cards)
-echo
+echoHand($dealer, "Dealer", true);
 
-if (condition) {
-	# code...
-}elseif (condition) {
-	# code...
-}elseif (condition) {
-	# code...
+// echo the player hand
+echoHand($player, "Player");
+echo "You have " . getHandTotal($player) . " points!";
+
+// allow player to "(H)it or (S)tay?" till they bust (exceed 21) or stay
+while (getHandTotal($player) <= 21) {
+	echo "(H)it or (S)tay ";
+	$input = getInput();
+
+if ($input == "H") {
+	$player = drawCard($player, $deck);
+	echo "Your score is " . getHandTotal($player) . PHP_EOL;
+	if ((getHandTotal($dealer) > getHandTotal($player)) && getHandTotal($player) > 21) {
+		echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+		echo "Dealer Wins";
+	}elseif ((getHandTotal($dealer) == getHandTotal($player)) && getHandTotal($player) > 21) {
+		echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+		echo "It is a Draw!";
+	}elseif (getHandTotal($player) > 21) {
+		echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+		echo "You busted Dealer Wins!";
+	}
+}elseif ($input == "S") {
+	echoHand($dealer, "Dealer");
+	while ((getHandTotal($dealer) < getHandTotal($player)) && (getHandTotal($dealer) < 21)) {
+		$dealer = drawCard($dealer, $deck);
+			echo "Your score is " . getHandTotal($player) . PHP_EOL;
+			echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+
+		if (getHandTotal($dealer) > 21) {
+			echo "Dealer busted you win!";
+			exit(0);
+		}
+	}
+	if (getHandTotal($dealer) > getHandTotal($player)) {
+		echo "Your score is " . getHandTotal($player) . PHP_EOL;
+		echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+		echo "Dealer Wins";
+		exit(0);
+	}elseif (getHandTotal($dealer) == getHandTotal($player)) {
+		echo "Your score is " . getHandTotal($player) . PHP_EOL;
+		echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+		echo "It is a Draw!";
+		exit(0);
+	}elseif ($getHandTotal($player) == 21) {
+		echo "Your score is " . getHandTotal($player) . PHP_EOL;
+		echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+		echo "You Win!";
+		exit(0);
+	}else {
+		echo "Your score is " . getHandTotal($player) . PHP_EOL;
+		echo "Dealer score is " . getHandTotal($dealer) . PHP_EOL;
+		echo "You Win!";
+		exit(0);
+	}
+}elseif ($input != ("H" || "S")) {
+	echo "(H)it or (S)tay";
+	$input = getInput();
 }
-if (condition) {
-	# code...
-}elseif (condition) {
-	# code...
-}elseif (condition) {
-	# code...
 }
 // at this point, if the player has more than 21, tell them they busted
 // otherwise, if they have 21, tell them they won (regardless of dealer hand)
